49e9ffc1fbcfe1259da9a4e0294a04a925b14ace
Allan <ramaasa@gmail.com>
2025-03-30 08:09:35 -0500

    initializate scripts

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+# config/settings.py
+
+"""
+Archivo de configuración para el proyecto.
+Contiene constantes y configuraciones globales.
+"""
+
+# Ruta relativa del archivo CSV con los datos (ten en cuenta que los precios están en moneda local)
+DATA_PATH = "data/base.csv"
